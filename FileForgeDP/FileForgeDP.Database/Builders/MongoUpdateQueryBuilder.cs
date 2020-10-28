add4ca57ba2e518c2bfc520c6f144d88c67ac206
Darkness190 <negocjator190@wp.pl>
2020-10-29 00:08:27 +0100

    Added builder for mongoDB update queries

---
+++ FileForgeDP/FileForgeDP.Database/Builders/MongoUpdateQueryBuilder.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileForgeDP.Database.Builders
+{
+    public class MongoUpdateQueryBuilder<T>
+    {
+        private List<UpdateDefinition<T>> mUpdates;
+        public MongoUpdateQueryBuilder()
+        {
+            mUpdates = new List<UpdateDefinition<T>>();
+        }
+        public static MongoUpdateQueryBuilder<T> Builder()
+        {
+            return new MongoUpdateQueryBuilder<T>();
+        }
+        //Patryk proszę nie patrz tu, nie patrz bo oślepniesz. Konrad nas opętał.
+        public MongoUpdateQueryBuilder<T> Add(string field, Object element)
+        {
+            mUpdates.Add(Builders<T>.Update.Set(field, element));
+            return this;
+        }
+        public UpdateDefinition<T> Build()
+        {
+            return Builders<T>.Update.Combine(mUpdates);
+        }
+    }
+}
