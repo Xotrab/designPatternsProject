bd73bbbab4672f9fca7cd9be26e178701fdb2a8b
Darkness190 <negocjator190@wp.pl>
2020-10-30 19:28:05 +0100

    Declared components and updated routing module

--- FileForgeDP/FileForgeDP/ClientApp/src/app/app-routing.module.ts
+++ FileForgeDP/FileForgeDP/ClientApp/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { HomeComponent } from './home/home.component';
+import { WorkspacesOverviewComponent } from './workspaces-overview/workspaces-overview.component';
 
-const routes: Routes = [{ path: '**', component: HomeComponent }];
+const routes: Routes = [
+    { path: 'admin', component: AdminPanelComponent},
+    { path: 'workspaces', component: WorkspacesOverviewComponent},   
+    { path: '**', component: HomeComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- FileForgeDP/FileForgeDP/ClientApp/src/app/app.module.ts
+++ FileForgeDP/FileForgeDP/ClientApp/src/app/app.module.ts
@@ -2,14 +2,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { LoginFormComponent } from './login-form/login-form.component';
+import { LoginFormComponent } from './home/login-form/login-form.component';
+import { RegisterFormComponent } from './home/register-form/register-form.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { WorkspaceComponent } from './workspaces-overview/workspace-component/workspace.component';
+import { WorkspacesOverviewComponent } from './workspaces-overview/workspaces-overview.component';
 
 @NgModule({
-    declarations: [AppComponent, NavMenuComponent, HomeComponent, LoginFormComponent],
+    declarations: [AppComponent, NavMenuComponent, HomeComponent, LoginFormComponent, RegisterFormComponent, WorkspacesOverviewComponent, WorkspaceComponent, AdminPanelComponent],
     imports: [
         BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
         HttpClientModule,
