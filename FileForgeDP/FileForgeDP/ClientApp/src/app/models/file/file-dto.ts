68aa7b0affcc5a2f780aec2b1843fe50b682e103
Xotrab <mokrylisc@gmail.com>
2020-11-07 12:42:39 +0100

    Created file model DTO

---
+++ FileForgeDP/FileForgeDP/ClientApp/src/app/models/file/file-dto.ts
@@ -0,0 +1,12 @@
+export class FileModelDto {
+    
+    public id: String;
+    public groupId: String;
+    public description: String;
+    public fileName: String;
+    public file: String;
+    public contentType: String;
+    public lastModificationDate: String;
+    public lastModifiedBy : String;
+
+}
