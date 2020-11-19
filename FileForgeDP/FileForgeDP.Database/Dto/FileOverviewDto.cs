bdc0ef8160731ce411c22447ad114f76707a7d94
Konrad Podgórski <konrad.podgorski1999@gmail.com>
2020-11-19 16:57:25 +0100

    Added dto to fetch file overview

---
+++ FileForgeDP/FileForgeDP.Database/Dto/FileOverviewDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileForgeDP.Database.Dto
+{
+    public class FileOverviewDto
+    {
+        public string Id { get; set; }
+
+
+        public string GroupId { get; set; }
+
+        public string Description { get; set; }
+
+
+        public string FileName { set; get; }
+
+        public string ContentType { get; set; }
+
+        public string LastModificationDate { get; set; }
+
+        public string LastModifiedBy { get; set; }
+    }
+}
