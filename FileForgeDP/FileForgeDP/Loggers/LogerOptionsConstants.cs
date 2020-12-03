850d4a3a19d83fd9d9de72a5a6bfe17f43fd3d63
Konrad Podgórski <konrad.podgorski1999@gmail.com>
2020-12-03 15:23:21 +0100

    Added Constans for logger options and for user actions

---
+++ FileForgeDP/FileForgeDP/Loggers/LogerOptionsConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileForgeDP.Loggers
+{
+    public static class LogerOptionsConstants
+    {
+        public static string USE_ACTION = "[ACTION]";
+        public static string USE_TIMESTAMP = "[TIMESTAMP]";
+        public static string USE_ACTION_TYPE = "[ACTION_TYPE]";
+        public static string USE_ACTOR = "[ACTOR]";
+        public static string USE_ACTION_STATUS = "[ACTION_STATUS]";
+    }
+}
