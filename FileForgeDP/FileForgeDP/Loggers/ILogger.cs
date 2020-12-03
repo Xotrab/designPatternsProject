549f99714ef0348bea0a7ff60675e6092f0f49db
Konrad Podgórski <konrad.podgorski1999@gmail.com>
2020-12-03 15:23:57 +0100

    Created interfaces for Logger and LoggerBuilder

---
+++ FileForgeDP/FileForgeDP/Loggers/ILogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileForgeDP.Loggers
+{
+    public interface ILogger
+    {
+        void Log(string log);
+    }
+}
---
+++ FileForgeDP/FileForgeDP/Loggers/ILoggerBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileForgeDP.Loggers
+{
+    public interface ILoggerBuilder
+    {
+        ILoggerBuilder Reset();
+        ILoggerBuilder BuildTimeStamp();
+        ILoggerBuilder BuilLogPaths(string path);
+        ILoggerBuilder BuildActor();
+        ILoggerBuilder BuildActionType();
+        ILoggerBuilder BuildAction();
+        ILoggerBuilder BuildActionStatus();
+        ILogger Build();
+    }
+}
