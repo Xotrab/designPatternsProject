e4d019c5ebd33c67c6faf81e79742e259c09ca76
Konrad Podgórski <konrad.podgorski1999@gmail.com>
2020-12-03 15:27:20 +0100

    Created ILogger implementation, AuditLogger  in order to log logs in specific log format. This logs format was suggested by RM.

---
+++ FileForgeDP/FileForgeDP/Loggers/AuditLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileForgeDP.Loggers
+{
+    public class AuditLogger : ILogger
+    {
+
+        internal string FormatString { get; set; }
+        internal string PathToFile { get; set; }
+
+        public AuditLogger(){}
+        public void Log(string log)
+        {   
+            saveToFIle(log, this.PathToFile);
+        }
+
+        public void Debug(string Actor, ActionEnum EnumActionType,string Action, string ActionStatus)
+        {
+            var logBuilder = new StringBuilder(FormatString);
+            logBuilder.Replace(LogerOptionsConstants.USE_ACTION, Action)
+                .Replace(LogerOptionsConstants.USE_ACTION_STATUS, ActionStatus)
+                .Replace(LogerOptionsConstants.USE_ACTOR, Actor)
+                .Replace(LogerOptionsConstants.USE_TIMESTAMP, DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"))
+                .Replace(LogerOptionsConstants.USE_ACTION_TYPE, Enum.GetName(typeof(ActionEnum), EnumActionType));
+
+            Log(logBuilder.ToString());
+            logBuilder.Clear();
+        }
+        public async void saveToFIle(string log, string path)
+        {   
+            using (StreamWriter sw = (File.Exists(path)) ? File.AppendText(path) : File.CreateText(path))             
+            {   
+                // Feel free to format output
+                sw.WriteLine(log);
+            }
+        }
+    }
+}
