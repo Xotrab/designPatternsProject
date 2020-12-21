69a02a3629275a5635dc332f1b7fe02bfc929466
Konrad Podgórski <konrad.podgorski1999@gmail.com>
2020-12-21 20:41:49 +0100

    Added interceptor in purpose of adding jwt token to every request

---
+++ FileForgeDP/FileForgeDP/ClientApp/src/app/security/authInterceptor.ts
@@ -0,0 +1,22 @@
+import { OAuthService } from 'angular-oauth2-oidc';
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    constructor(private oauthService: OAuthService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        // Get the auth token from the service.
+        const authHeader = this.oauthService.authorizationHeader();
+        console.log(JSON.stringify(this.oauthService.getIdentityClaims()));
+        // Clone the request and replace the original headers with
+        // cloned headers, updated with the authorization.
+        const authReq = req.clone({
+            headers: req.headers.set('Authorization', authHeader),
+        });
+
+        // send cloned request with header to the next handler.
+        return next.handle(authReq);
+    }
+}
